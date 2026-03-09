0139e0eac11a37cc762d6d6995fb13f7b31e44e7
KimKihoon <leonardkim1209@gmail.com>
2026-03-09 21:50:04 +0900

    fix: Vertex를 DataType.h에 맞춰 FVertex로 수정

--- Project_Week01/UPrimitive.h
+++ Project_Week01/UPrimitive.h
@@ -1,39 +1,25 @@
 #pragma once
 #include <vector>
-#include "DataType.h" // 우리가 만든 규격집 포함
+#include "datatype.h"
 
-class UPrimitive {
+class UPrimitive 
+{
 protected:
-	std::vector<Vertex> vertices; // 정적 모양 데이터 (VB용)
-	FVector Location;             // 실시간 위치 데이터 (FVector 기반)
-	float Scale;                  // 크기 배율
+	std::vector<FVertex> vertices;
+	FVector Location;
+	float Scale;
 
 public:
-	UPrimitive() : Location(0, 0, 0), Scale(1.0f) {}
-	virtual ~UPrimitive() {} // 상속을 위한 가상 소멸자
+	UPrimitive();
+	virtual ~UPrimitive(); // 상속을 위한 가상 소멸자
 
 	// 1. 매니저(물리 엔진)가 사용할 함수
-	void SetLocation(const FVector& InLocation)
-	{
-		Location = InLocation;
-	}
-
-	FVector GetLocation() const
-	{
-		return Location;
-	}
+	void SetLocation(const FVector& InLocation);
+	FVector GetLocation() const;
 
 	// 2. 렌더러가 사용할 데이터 제공 함수
-	const std::vector<Vertex>& GetVertices() const
-	{
-		return vertices;
-	}
+	const std::vector<FVertex>& GetVertices() const;
 
 	// 상수 버퍼(CB)에 넘겨줄 최종 패키지 생성
-	FConstants GetConstants() const
-	{
-		FConstants Data(Location);
-		Data.scale = Scale;
-		return Data;
-	}
+	FConstants GetConstants() const;
 };
