79c72d0ff4bb7f63fe3c102f17e07400c32d6cc6
Park SangHyeok <tkdgurehf00@gmail.com>
2026-03-10 13:31:26 +0900

    PlayerInputFirstCommit

---
+++ Project_Week01/PlayerInput.cpp
@@ -0,0 +1,46 @@
+#include "PlayerInput.h"
+#include <Windows.h>
+
+PlayerInput::PlayerInput() : io(ImGui::GetIO())
+{
+}
+
+PlayerInput::~PlayerInput()
+{
+}
+
+FVector PlayerInput::GetMousePosition()
+{
+	//0 ~ display size
+    float mouseX = io.MousePos.x;
+    float mouseY = io.MousePos.y;
+
+	float width = io.DisplaySize.x;
+	float height = io.DisplaySize.y;
+
+
+	//-1 ~ 1 사이 좌표 리턴
+	return FVector((mouseX / width - 0.5f) * 2.f, (mouseY / height - 0.5f) * 2.f, 1.f);
+}
+
+//클릭
+bool PlayerInput::IsMouseClicked(int mouseButton)
+{
+	return ImGui::IsMouseClicked(mouseButton);
+}
+
+//누르고 있음
+bool PlayerInput::IsMouseDown(int mouseButton)
+{
+	return ImGui::IsMouseDown(mouseButton);
+}
+
+//뗌
+bool PlayerInput::IsMouseReleased(int mouseButton)
+{
+	return ImGui::IsMouseReleased(mouseButton);
+}
+
+
+
+
