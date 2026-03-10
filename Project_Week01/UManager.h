10ae843f1cb18f3b0f549c61cef0d04602e027c2
an_yan_yang <yanggogi1423@gmail.com>
2026-03-10 10:38:07 +0900

    Add : RunState, Refactor

--- Project_Week01/UManager.h
+++ Project_Week01/UManager.h
@@ -6,11 +6,16 @@
 #include <string>
 #include <sstream>
 
+#include "Probe.h"
+
+
 //	Constants
 constexpr float TopBorder = -1.f;
 constexpr float BottomBorder = 1.f;
 constexpr float LeftBorder = -1.f;
-constexpr float TopBorder = 1.f;
+constexpr float RightBorder = 1.f;
+
+constexpr size_t PlanetListReservedSize = 10;
 
 #pragma region __GAME_STATE__
 
@@ -18,9 +23,14 @@ constexpr float TopBorder = 1.f;
 enum class ERunstate
 {
 	ERS_Main,
-	ERS_InGame,
+	ERS_InGameReady,
+	ERS_InGameRun,
 	ERS_Ending	//	플레이어의 사망 || 게임 클리어
 };
+
+#pragma endregion
+
+#pragma region __STAGE_INFO__
 
 enum class EStage
 {
@@ -28,6 +38,16 @@ enum class EStage
 	ES_Stage1,
 	ES_Stage2,
 	ES_Stage3
+};
+
+//	스테이지에 대한 정보 저장
+struct FStageInfo
+{
+	unsigned int Index;
+	float MaxTime;
+	//	1. 행성의 종류(idx 구분)와 개수에 대한 정보
+	//	2. 장애물 위치 및 종류(idx), 개수 정보 -> 위치, 개수를 pair (혹은 개수만)
+	//	3. 탈출구 위치
 };
 
 #pragma endregion
@@ -41,15 +61,22 @@ private:
 	EStage CurStage;
 
 	//	Time
-	float RemainTime;
+	float RemainTimer;
+
+	//	Boot and Destroy - 반복 호출을 막기 위한 flag
+	bool bBootDone;
+	bool bIsAlreadyDestroy;
 
 private:
 	const std::string FileName;
 	std::vector<std::pair<std::string, unsigned int>> ScoreList;
 
 	/* GameObjects */
-public:
-	//	행성의 종류가 dynamic할 수 있음을 염두
+	Probe* Player;
+	std::vector<UPrimitive> PlanetList;	//	이후에 template 수정할 수도 있음
+
+
+	
 
 	/* Game Management */
 private:
@@ -59,7 +86,8 @@ private:
 
 	//	Scene Initialization
 	void MainInit();
-	void InGameInit();
+	void InGameReadyInit();
+	void InGameRunInit();	//	사실상 Simulation Start
 	void EndingInit();
 
 	//	Stage Progress
@@ -67,7 +95,14 @@ private:
 	void InitGameObjects();
 	void ClearGameObjects();
 
+	//	Game Logic
+	//	행성 배치
+
+
 	/* Non-game Management */
+	void BootGame();
+	void DestroyGame();
+
 	//	File Load
 	void LoadScore();
 	void SaveScore();
@@ -75,18 +110,23 @@ private:
 
 
 public:
+	/* Cons, Des */
 	UManager()
 		: CurRunState(ERunstate::ERS_Main), CurStage(EStage::ES_None),
-		FileName("ranking.txt")
+		FileName("ranking.txt"),
+		bBootDone(false), bIsAlreadyDestroy(false)
 	{
-		LoadScore();
+		BootGame();
 	}
 	~UManager()
 	{
-		SaveScore();
+		DestroyGame();
 	}
 
 	void Update(float deltaTime);
 
+	/* Getter, Setter */
+	const Probe& GetProbe() const { return (*Player);  }
+	bool Startable() const { return bBootDone; }
 
 };
