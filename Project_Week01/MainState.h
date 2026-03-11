cd89a7befefcfda74897e5b0c7c6ab21a121535b
an_yan_yang <yanggogi1423@gmail.com>
2026-03-11 21:14:50 +0900

    Feat : Main Scene

--- Project_Week01/MainState.h
+++ Project_Week01/MainState.h
@@ -10,6 +10,17 @@ public:
 	IState* Update(URenderer* renderer) override;
 	void OnExit() override;
 
+	//	LOGO Animation
+	const float TopOffsetY = -25.f;
+	const float BottomOffsetY = 25.f;
+	float OffsetY = 0.f;
+	float DY = -0.5f;
+
+	UIFrame* LogoFrame;
+
+	//	Contributors
+	UIFrame* InfoFrame;
+	//bool bInfoVisible = false;
 
 	//예시용
 	IState* GoToExampleState();
--- Project_Week01/UResourceManager.h
+++ Project_Week01/UResourceManager.h
@@ -42,6 +42,9 @@ public:
 	//	Fonts Resources
 	ImFont* FontLogo;
 	ImFont* FontDefault;
+	ImFont* FontInfoBold;
+	ImFont* FontInfoRegular;
+	ImFont* FontInfoLight;
 	
 public:
 	UResourceManager() : SRVBackground(nullptr), SRVInGamePanel(nullptr), SRVLeaderBoardPanel(nullptr), SRVButtonSprite(nullptr),
