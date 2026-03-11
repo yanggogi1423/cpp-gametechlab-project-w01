5474122a85cdfe58a4eead5dce4a19ec8ee0fd26
Park SangHyeok <tkdgurehf00@gmail.com>
2026-03-11 09:52:45 +0900

    Add ImGui Static Init, ShutDown

--- Project_Week01/UIManager.cpp
+++ Project_Week01/UIManager.cpp
@@ -14,6 +14,21 @@ UIFrame& UIManager::CreateFrame(const std::string& title)
 	return *UIFrameList.back();
 }
 
+void UIManager::InitImGui(HWND hWnd, URenderer* renderer)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplWin32_Init((void*)hWnd);
+	ImGui_ImplDX11_Init(renderer->Device, renderer->DeviceContext);
+}
+
+void UIManager::ShutdownImGui()
+{
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+}
+
 void UIManager::Render()
 {
 	ImGui_ImplDX11_NewFrame();
